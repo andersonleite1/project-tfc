6df3179d1e858f8ffcb29ea0314c65cb81fe9f0f
Anderson Leite <andersonleite.dev@gmail.com>
2022-07-14 11:09:06 -0300

    feat(util): create leaderboads

---
+++ app/backend/src/utils/leaderboards.ts
@@ -0,0 +1,73 @@
+import IMatch from '../interfaces/IMatch';
+
+const teamData = {
+  name: '',
+  totalPoints: 0,
+  totalGames: 0,
+  totalVictories: 0,
+  totalDraws: 0,
+  totalLosses: 0,
+  goalsFavor: 0,
+  goalsOwn: 0,
+  goalsBalance: 0,
+  efficiency: 0,
+};
+
+const resetTeamData = () => {
+  teamData.totalPoints = 0;
+  teamData.totalGames = 0;
+  teamData.totalVictories = 0;
+  teamData.totalDraws = 0;
+  teamData.totalLosses = 0;
+  teamData.goalsFavor = 0;
+  teamData.goalsOwn = 0;
+  teamData.goalsBalance = 0;
+  teamData.efficiency = 0;
+};
+
+const addDataVictories = (homeTeamGoals: number, awayTeamGoals: number) => {
+  teamData.totalPoints += 3;
+  teamData.totalVictories += 1;
+  teamData.goalsFavor += homeTeamGoals;
+  teamData.goalsOwn += awayTeamGoals;
+};
+
+const addDataDraws = (homeTeamGoals: number, awayTeamGoals: number) => {
+  teamData.totalPoints += 1;
+  teamData.totalDraws += 1;
+  teamData.goalsFavor += homeTeamGoals;
+  teamData.goalsOwn += awayTeamGoals;
+};
+
+const addDataLoses = (homeTeamGoals: number, awayTeamGoals: number) => {
+  teamData.totalLosses += 1;
+  teamData.goalsFavor += homeTeamGoals;
+  teamData.goalsOwn += awayTeamGoals;
+};
+
+const calculateEfficiency = (totalPoints: number, totalGames: number) => {
+  const efficiency = (totalPoints / (totalGames * 3)) * 100;
+  return Number(efficiency).toFixed(2);
+};
+
+const calculateTotalPointsHome = (matches: IMatch[]) => {
+  matches.forEach(({ homeTeamGoals, awayTeamGoals }) => {
+    if (homeTeamGoals > awayTeamGoals) addDataVictories(homeTeamGoals, awayTeamGoals);
+    if (homeTeamGoals === awayTeamGoals) addDataDraws(homeTeamGoals, awayTeamGoals);
+    if (homeTeamGoals < awayTeamGoals) addDataLoses(homeTeamGoals, awayTeamGoals);
+  });
+};
+
+const calculateStatisticsTeamPlayedHome = (name: string, matches: IMatch[]) => {
+  if (name !== teamData.name) resetTeamData();
+  teamData.name = name;
+  calculateTotalPointsHome(matches);
+  teamData.totalGames += 1;
+  teamData.goalsBalance = teamData.goalsFavor - teamData.goalsOwn;
+  const efficiency = calculateEfficiency(teamData.totalPoints, teamData.totalGames);
+  teamData.efficiency = Number(efficiency);
+
+  return teamData;
+};
+
+export default calculateStatisticsTeamPlayedHome;
